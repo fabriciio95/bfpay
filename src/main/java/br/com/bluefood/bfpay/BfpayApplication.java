7ddb192b2fbda7bc664a73bf206c0224fe5ca52a
fabriciio95 <fabriciusiqueira@gmail.com>
2020-10-26 12:10:20 -0300

    Projeto criado

---
+++ src/main/java/br/com/bluefood/bfpay/BfpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.bluefood.bfpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bluefood/bfpay/BfpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bluefood.bfpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
